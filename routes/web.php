37bd78947183df666126cfd477715122a315c9cf
arian <arianpourinfo@gmail.com>
2017-01-30 11:37:27 +0800

    init- and fixed

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/suppliers','SupplierController@index');
+    Route::post('/suppliers','SupplierController@store');
+    Route::get('/suppliers/{id}','SupplierController@show');
+    Route::post('/suppliers/products','SupplierController@showProducts');
+    Route::delete('/suppliers/{id}','SupplierController@destroy');
+
+    Route::get('/products','ProductController@index');
+    Route::post('/products','ProductController@store');
+    Route::get('/products/{id}','ProductController@show');
+    Route::delete('/products/{id}','ProductController@destroy');
+
+
+});
